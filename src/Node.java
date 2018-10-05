682f7866017be8911dea4c09f7242133693b949b
Adam Rubic <arubic@asu.edu>
2018-10-04 19:29:13 -0700

    node class and methods

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,43 @@
 
-public class Node {
+public class Node 
+{
+	private int size;
+	private char[] dependencies;
+	private String name;
+	
+	public Node(){
+		size = 0;
+		name = " ";
+	}
+	
+	public Node(int num, String newName, char[]nodeDependencies){
+		size = num;
+		name = newName;
+		dependencies = nodeDependencies;
+	}
+	public void setSize(int num){
+		
+		size = num;
+	}
 
+	public void setDependencies(char[] nodeDependencies){
+		dependencies = nodeDependencies;
+	}
+	
+	public void setName(String newName){
+		name = newName;
+	}
+	
+	public int getSize(){
+		return size;
+	}
+	
+	public char[] getDependencies(){
+		return dependencies;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
 }
