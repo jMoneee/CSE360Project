61de8d5ccaf457657c3f0a7febd089e10f8972fd
Adam Rubic <arubic@asu.edu>
2018-10-11 18:21:04 -0700

    big changes

--- src/gui.java
+++ src/gui.java
@@ -9,6 +9,8 @@ import javax.swing.JTextField;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+
 import java.awt.Insets;
 import java.awt.event.ActionListener;
 
@@ -18,6 +20,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.JSplitPane;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import java.awt.Color;
 import javax.swing.border.BevelBorder;
@@ -39,7 +42,8 @@ public class gui extends JFrame {
 	private JTextArea textField_3;
 	private JTextArea textField_4;
 	Path p = new Path();
-
+	ArrayList <String> dependencyList = new ArrayList<String>();
+	ArrayList<Node> joe;
 	/**
 	 * Launch the application.
 	 */
@@ -76,8 +80,36 @@ public class gui extends JFrame {
 		JButton btnAbout = new JButton("About");
 
 		btnAbout.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("about clicked");
+	public void actionPerformed(ActionEvent e) {
+					
+		String about= "The purpose of the program is to analyse an inputed set of nodes of a network diagram and determine all the paths as well as information about said paths. \r\n" + 
+				"\r\n" + 
+				"This project was created by the following people:\n" + 
+				"\n" + 
+				"Joseph Larsen\n" + 
+				"Adam Rubic\n" + 
+				"Kaisser Kelcho\n" + 
+				"Lukas Cronin\n" + 
+				"";
+					
+					JOptionPane pane = new JOptionPane(about);
+					JDialog window = pane.createDialog("About ");
+					window.setSize(1800,300);
+					window.show();
+					
+					PathMaker pathM = new PathMaker();
+	                ArrayList<Path> pathList;
+	                pathList = pathM.MakePaths(joe);
+	                
+	                System.out.println("path size"+pathList.size());
+	                for(int i=0; i<pathList.size();i++)
+	                {
+	                	for(int j=0; j<pathList.get(i).getActivities().size();j++)
+	                	{
+	                		System.out.println(pathList.get(i).getActivities().get(j).getName());
+	                	}
+	                	System.out.println("");
+	                }
 			}
 		});
 
@@ -86,10 +118,22 @@ public class gui extends JFrame {
 		JButton button = new JButton("Help");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+					
+					String helpBut ="The input tab contains 3 text input fields for inputting activites, one for an activity’s name, duration and dependencies. \n"
+							+ "The text from these fields is taken from these fields once the ‘Enter Activity’ button is pressed and used to create the activities in the list. "
+							+ "\nAs activities are entered they will appear in the field below it, which is a list of all entered activities. "
+							+ "\nThe path list will be displayed on the Output tab and is updated every time a new node is entered." + 
+							"\n" + 
+							"The Output tab will only have one field which will display the list of paths and all needed information about the paths. The output tab will be update every time a new node is added.\r\n" + 
+							"";
+					
+					JOptionPane pane = new JOptionPane(helpBut);
+					JDialog window = pane.createDialog("Help");
+					window.setSize(1800,300);
+					window.show();
+				}
 
-				System.out.println("help clicked");
-
-			}
+			
 		});
 		panel.add(button);
 		
@@ -100,6 +144,7 @@ public class gui extends JFrame {
 				System.out.println("restart clicked");
 				p = new Path();
 				textField_4.setText("");
+				textField_3.setText("Activity Name:\t\t\tPredicessor(s):\t\t\tDuration:\n");
 			}
 		});
 
@@ -163,21 +208,50 @@ public class gui extends JFrame {
 		btnEnterActivity.setBounds(494, 64, 141, 35);
 		panel_1.add(btnEnterActivity);
 
+	
+		
 		btnEnterActivity.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("node created");
 				ArrayList<String> depen = new ArrayList<String>();
 				if(textField_2.getText().equals(""))
-					textField_2.setText(" ");
+				{
+					
+				}
+				else
+				{
 				for(int i=0; i<textField_2.getText().length();i++)
 					depen.add(textField_2.getText().substring(i, i+1));
+				}
 				Node n = new Node(Integer.parseInt(textField_1.getText()), textField.getText(), depen);
+				System.out.println("depends" +n.hasDependencies());
 				p.addNode(n);
 				PathMaker m = new PathMaker();
 				String names="",input=textField_3.getText();
-				ArrayList<Node> joe = p.getActivities();
+				joe = p.getActivities();
 				for(int i=0; i<joe.size();i++)
 					names+=joe.get(i).getName();
+
+                         	
+				
+                	
+
+               
+               
+             //  System.out.println(m.findFirst(joe));
+              // System.out.println(m.findDependencies(m.findFirst(joe),joe));
+                Path temp;
+               /* for (int i = 1; i < pathList.size(); i++) {
+                    for(int j = i ; j > 0 ; j--){
+                        if(pathList.get(j).getDuration() < pathList.get(j-1).getDuration()){
+                            temp = pathList.get(j);
+                            pathList.set(j, pathList.get(j-1));
+
+                            pathList.set(j-1, temp);
+                        }
+                    }
+                }*/
+				
 				
 				input+=n.getName()+"\t\t\t"+n.printDependencies()+" \t\t\t"+n.getSize()+"\n";
 				textField_3.setText(input);
